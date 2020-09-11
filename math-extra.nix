28b2877f270e54b0adbd24b9370e3f419e58ea9b
Xavier Francisco <xavier.n.francisco@gmail.com>
2020-09-11 02:14:12 +0100

    Add some more functions to utils (#2)

--- math-extra.nix
+++ math-extra.nix
@@ -9,4 +9,53 @@ let lib = import <nixpkgs/lib>;
     odd = n: mod n 2 == 1;
     even = n: mod n 2 == 0;
 
-in { inherit abs even mod odd sum; }
+    slope = (fst: snd:
+      let
+        rise = snd.y - fst.y;
+        run = snd.x - fst.x;
+      in
+        if run != 0 then rise / run
+                    else null
+    );
+    manhattanDistance = (fst: snd: abs (snd.x - fst.x) + abs (snd.y - fst.y));
+
+    quadrant = (vec:
+      if vec.x > 0 && vec.y > 0 then
+        1
+      else if vec.x < 0 && vec.y > 0 then
+        2
+      else if vec.x < 0 && vec.y < 0 then
+        3
+      else if vec.x > 0 && vec.y < 0 then
+        4
+      # not really quadrant anymore
+      else if vec.x == 0 && vec.y == 0 then
+        0
+      else if vec.y == 0 && vec.x > 0 then
+        -1
+      else if vec.x == 0 && vec.y > 0 then
+        -2
+      else if vec.y == 0 && vec.x < 0 then
+        -3
+      else if vec.x == 0 && vec.y < 0 then
+        -4
+      else
+        abort "Aaaaaahhhh"
+    );
+
+    semiaxis = (vec:
+      if vec.x == 0 && vec.y == 0 then
+        0
+      else if vec.y == 0 && vec.x > 0 then
+        1
+      else if vec.x == 0 && vec.y > 0 then
+        2
+      else if vec.y == 0 && vec.x < 0 then
+        3
+      else if vec.x == 0 && vec.y < 0 then
+        4
+      else
+        null
+    );
+
+in { inherit abs even mod odd sum slope manhattanDistance; }
